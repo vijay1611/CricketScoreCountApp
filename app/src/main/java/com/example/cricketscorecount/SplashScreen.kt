a45fdbe1dfa935f2c7f1b6c166ad1a667b349724
Rahul S <rahul.s@ctlep.local>
2023-07-12 15:05:10 +0530

    (splash screen launch)Initial commit

---
+++ app/src/main/java/com/example/cricketscorecount/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.example.cricketscorecount
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreen : AppCompatActivity() {
+    //var handler : Handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            startActivity(Intent(this,MainActivity::class.java))
+        },3000)
+    }
+
+}
